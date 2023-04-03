ca2f36d46b88ea6e355d433192a40f1ffa547b77
fancn21th <fancn21th@gmail.com>
2023-04-03 15:48:26 +0800

    remove js files

--- packages/utils/tests/http-error.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var utils_1 = require("@shlack/utils");
-describe("HTTPError tests", function () {
-    test("creation", function () {
-        var e = new utils_1.HTTPError({ status: 404, statusText: "Not Found" }, "Mock error for testing");
-        expect(e.kind).toBe(utils_1.HTTPErrorKind.Client);
-    });
-});
